29414119e48b6acc528d89b8bce36f0da1a2a9ef
TesyuTesyu <ntrt0118@gmail.com>
2025-02-02 15:30:02 +0900

    Delete PEEC_2d_any_shape_v5 directory

--- PEEC_2d_any_shape_v5/ffmpeg_video_maker_v1.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-input_path=r"???\PEEC_work_space\fig"
-out_path=input_path+"\\out_voltage.mp4"
-
-command = f'ffmpeg -framerate 5 -i "{input_path+"\\"+"%03d_voltage.png"}" -vcodec libx264 -pix_fmt yuv420p -r 15 {out_path}'
-
-subprocess.call(command, shell=True)
